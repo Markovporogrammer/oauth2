ff76a148d88ae274a6b7f675741986ee0ad445a5
mazepeng-jwk <mazepeng-jwk@360shuke.com>
2021-09-18 17:48:43 +0800

    first commit

---
+++ src/main/java/com/oauth2/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@Order(2)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+ 
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+ 
+        return super.userDetailsService();
+    }
+ 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq") //123
+                .roles("admin")
+                .and()
+                .withUser("sang")
+                .password("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq") //123
+                .roles("user");
+    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.antMatcher("/oauth/**").authorizeRequests()
+//                .antMatchers("/oauth/**").permitAll()
+//                .and().csrf().disable();
+//    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .requestMatchers().antMatchers("/oauth/**","/login/**","/logout/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**").authenticated()
+                .and()
+                .formLogin().permitAll(); //新增login form支持用户登录及授权
+    }
+}
